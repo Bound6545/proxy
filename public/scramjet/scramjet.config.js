32f86031559df6a15cf0a7220ac4550344839b71
Bound6545 <starbetcopyright@gmail.com>
2026-01-28 17:19:30 -0600

    Update scramjet.config.js

--- public/scramjet/scramjet.config.js
+++ public/scramjet/scramjet.config.js
@@ -1,1 +1,16 @@
-(()=>{self.__scramjet$config={prefix:"/service/",codec:self.__scramjet$codecs.plain,config:"/scramjet/scramjet.config.js",bundle:"/scramjet/scramjet.bundle.js",worker:"/scramjet/scramjet.worker.js",client:"/scramjet/scramjet.client.js",codecs:"/scramjet/scramjet.codecs.js"};})();
+// Scramjet configuration
+self.$scramjet = {
+    version: {
+        build: "latest",
+        type: "service"
+    },
+    config: {
+        prefix: "/service/",
+        files: {
+            wasm: "/scramjet/scramjet.wasm",
+            worker: "/scramjet/scramjet.worker.js",
+            client: "/scramjet/scramjet.client.js",
+            sync: "/scramjet/scramjet.sync.js"
+        }
+    }
+};
